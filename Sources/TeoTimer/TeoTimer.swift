93aff912252e1c6267fe13728254b2a4e6f7f436
teo.kim <cooo002@naver.com>
2023-12-08 12:42:25 +0900

    Initial Commit

---
+++ Sources/TeoTimer/TeoTimer.swift
@@ -0,0 +1,14 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+import SwiftUI
+
+public struct TeoTimerView: View {
+    
+    public var body: some View {
+        Text("TeoTimerView")
+    }
+}
+
+#Preview {
+    TeoTimerView()
+}
